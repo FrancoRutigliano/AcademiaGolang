334f0cfcf4184756b77d5c53f7250b9639475f32
Franco Rutigliano <francorutigliano11@gmail.com>
2024-02-06 19:21:29 -0300

    Ejercicio estructuras

---
+++ Ejercicios/StructClase/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var opc int
+	fmt.Print("Sr Administrador que quiere hacer \n1)Agregar un nuevo Deportivo\n2)Agregar una nueva camioneta\n3)Mostrar todos los autos\n4)Eliminar algún deportivo\n5)Eliminar una camioneta\n") // modificalo
+
+	fmt.Print("INGRESAR UNA OPCION: ")
+	fmt.Scanln(&opc)
+
+	switch opc {
+	case 1:
+
+	}
+}
